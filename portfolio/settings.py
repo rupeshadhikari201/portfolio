44cb859d70cb26aaabc8f03ed6c11307aaf26eba
rupeshadhikari201 <137412902+rupeshadhikari201@users.noreply.github.com>
2024-12-12 01:35:18 +0530

    Update settings.py
    added portfolio-l1te.onrender.com to allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-aphi_$9n%7!yfui9zs(&6n(k0m53ffy00&6y6ihcr7dctf+^fe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-l1te.onrender.com',]
 
 
 # Application definition
