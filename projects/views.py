b4db29fb9f45a11f3cb6b6a3096dad040181e3d2
Rupesh <21bcs11201@gmail.com>
2025-01-13 17:59:07 +0530

    add

--- projects/views.py
+++ projects/views.py
@@ -4,4 +4,5 @@ from .models import Projects
 # Create your views here.
 def projects(request):
     projects = Projects.objects.all()
+    print(projects)
     return render(request, 'projects/projects.html', {'projects' : projects})
